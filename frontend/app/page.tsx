cfd08a8a4804da55f8c7c69df4e4d04863854fd3
JeremyNazareth <jer.reyesr@outlook.com>
2026-03-05 17:20:16 -0300

    style: add JetBrains font

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { MoveRight, MoveLeft } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex min-h-screen ml-80 dark:bg-primary">
@@ -7,15 +7,15 @@ export default function Home() {
         <div className="flex items-center gap-2 px-1">
           <div className="label">
             <p>
-              ⬅️
+              <MoveLeft />
             </p>
           </div> 
           <div className="label">
             <p>
-              ➡️
+              <MoveRight />
             </p>
           </div>
-          <div className="w-full h-8.75">
+          <div className="w-full h-8.75 max-w-250">
             <p className="label">Hospital/Clientes/🙍‍♂️Persona</p>
           </div>
         </div>
