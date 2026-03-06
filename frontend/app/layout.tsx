b091a4ff425174e5b586f1e28fb650b2a5ac23a4
JeremyNazareth <jer.reyesr@outlook.com>
2026-03-06 17:27:26 -0300

    style: add styles to files-navegator

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -14,8 +14,8 @@ export default function RootLayout({
           <main>
             <h1 className="text-center decoration-none">Easy Mock</h1>
             <div>
-              <h1 className="text-center bg-primary mt-20 p-1 rounded-t-sm">Jeremy</h1>
-              <ul className="folder-navegator bg-content rounded-b-sm pl-6 py-4 flex flex-col gap-2">
+              <h1 className="text-center bg-primary mt-20 p-1 rounded-t-[7px]">Jeremy</h1>
+              <ul className="folder-navegator bg-content rounded-b-[7px] pl-6 py-4 flex flex-col gap-2">
                 <li><Folder size={22}/> Hospital</li>
                 <li><Folder />Cliente</li>
                 <li><Folder />Autos</li>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { MoveRight, MoveLeft } from "lucide-react";
+import { MoveRight, MoveLeft, UserRound } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex min-h-screen ml-80 dark:bg-primary">
-      <main className="w-full p-2">
-        <div className="flex items-center gap-2 px-1">
+      <main className="w-full py-2 px-4 flex flex-col gap-2">
+        <div className="flex items-center gap-2 max-w-250">
           <div className="label">
             <p>
               <MoveLeft />
@@ -15,11 +15,25 @@ export default function Home() {
               <MoveRight />
             </p>
           </div>
-          <div className="w-full h-8.75 max-w-250">
-            <p className="label">Hospital/Clientes/🙍‍♂️Persona</p>
+          <div className="w-full h-8.75">
+            <p className="label flex">Hospital/Clientes/<p className="flex gap-1"><UserRound size={18} />Persona</p></p>
           </div>
         </div>
-  
+        <div className="flex gap-2">
+          <div className="label w-fit"><p className="flex items-center gap-1"><UserRound size={20} />Persona de Clientes</p> </div>
+          <div className="label w-fit"><p className="flex items-center gap-1">32 <UserRound size={20} />Personas</p> </div>
+        </div>
+        <ul className="files-navegator max-w-250 flex flex-col rounded-[7px] overflow-hidden">
+          <li className="bg-layout">
+            <p className="flex gap-1.5"> <UserRound size={20}/> José </p>
+          </li>
+          <li className="bg-content">
+            <p className="flex gap-1.5"> <UserRound size={20}/> María </p>
+          </li>
+          <li className="bg-layout">
+            <p className="flex gap-1.5"> <UserRound size={20}/> José </p>
+          </li>
+        </ul>
       </main>
     </div>
   );
